2028031ff2a239215cf9e54abeabf3822e47e090
Hoon Jeong <yong08211@gmail.com>
2024-06-10 22:25:02 -0400

    add redirect, form

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,15 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
+router.get("/new", function (req, res) {
+  res.render("form");
+});
+router.post("/new", function (req, res) {
+  messages.push({
+    text: req.body.textMsg,
+    user: req.body.name,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
 module.exports = router;
